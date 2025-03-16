d36d45cec983ef7626f2046a9daeab92058b2d63
Svetlana Kalashnikova <serebsveta@rambler.ru>
2025-03-17 03:02:09 +0800

    added launcher module

---
+++ main.py
@@ -0,0 +1,5 @@
+from src.user_interface import user_interface
+
+if __name__ == "__main__":
+    print("""Для того чтобы найти и сохранить интересующие Вас вакансии, ответьте на ряд вопросов.""")
+    user_interface()
